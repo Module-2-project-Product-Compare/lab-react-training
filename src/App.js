7eacb169bd549a8f41f9d01d2c7b23f5651e8b91
Giovanni Bedoya <giovedoya@gmail.com>
2023-02-26 15:13:40 +0100

    'Iteration 1 | Component: IdCard'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-
+import IdCard from './components/IdCard';
 import './App.css';
+
+const userIdCard = [
+  {
+    lastName: 'Doe',
+    firstName: 'John',
+    gender: 'male',
+    height: 1.78,
+    birth: new Date("1992-07-14"),
+    picture: "https://randomuser.me/api/portraits/men/44.jpg"
+  },
+  {
+    lastName: 'Delores',
+    firstName: 'Obrien',
+    gender: 'female',
+    height: 1.72,
+    birth: new Date("1988-05-11"),
+    picture: "https://randomuser.me/api/portraits/women/44.jpg"
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <h1>Lab react</h1>
+      <h1>User profile</h1>
+      {userIdCard.map((elem, i) => {
+        return (
+          <IdCard key={i} user={elem} />
+        )
+      })}
     </div>
   );
 }
---
+++ src/components/IdCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+   
+    function Idcard(props) {
+
+        const {user: {lastName, firstName, gender, height, birth, picture}} = props;
+        return (
+            <div className='card-user'>
+                <p><span>First name:</span> {firstName}</p>
+                <p><span>Last name:</span> {lastName}</p>
+                <p><span>Gender:</span> {gender}</p>
+                <p><span>Height:</span> {height}</p>
+                <p><span>Birthday:</span> {birth.toDateString()}</p>
+                <img src={picture} className="profile" alt="profile"/>
+            </div>
+        );
+    };
+
+export default Idcard;
